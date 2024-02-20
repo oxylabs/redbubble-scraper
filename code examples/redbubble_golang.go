7d9d414f3b92ea0bb35a9bab938d11dd7d536690
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:32:23 +0200

    Adding code examples/redbubble_golang.go

---
+++ code examples/redbubble_golang.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	const Username = "YOUR_USERNAME"
+	const Password = "YOUR_PASSWORD"
+
+	payload := map[string]string{
+		"source": "universal_ecommerce",
+		"url": "https://www.redbubble.com/g/t-shirts",
+	}
+
+	jsonValue, _ := json.Marshal(payload)
+
+	client := &http.Client{}
+	request, _ := http.NewRequest("POST",
+		"https://realtime.oxylabs.io/v1/queries",
+		bytes.NewBuffer(jsonValue),
+	)
+
+	request.SetBasicAuth(Username, Password)
+	response, _ := client.Do(request)
+
+	responseText, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(responseText))
+}

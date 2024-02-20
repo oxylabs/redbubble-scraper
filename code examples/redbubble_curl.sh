2fd46d8ef01832dfaa65dcb51288c3476293e45e
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:32:20 +0200

    Adding code examples/redbubble_curl.sh

---
+++ code examples/redbubble_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.redbubble.com/g/t-shirts"}'

08c1018d7a77d905020ba265650e5133b8cc9ac4
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:32:22 +0200

    Adding code examples/redbubble_python.py

---
+++ code examples/redbubble_python.py
@@ -0,0 +1,20 @@
+import requests
+from pprint import pprint
+
+# Structure payload.
+payload = {
+    'source': 'universal_ecommerce',
+    'url': 'https://www.redbubble.com/g/t-shirts'
+}
+
+# Get response.
+response = requests.request(
+    'POST',
+    'https://realtime.oxylabs.io/v1/queries',
+    auth=('user', 'pass1'),
+    json=payload,
+)
+
+# Instead of response with job status and results url, this will return the
+# JSON response with the result.
+pprint(response.json())

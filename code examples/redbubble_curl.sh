c884e3158091d444435fab57708177cbc1e1ce48
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 12:49:06 +0300

    Update redbubble_curl.sh

--- code examples/redbubble_curl.sh
+++ code examples/redbubble_curl.sh
@@ -1,4 +1,4 @@
 curl --user user:pass \
 'https://realtime.oxylabs.io/v1/queries' \
 -H "Content-Type: application/json" \
--d '{"source": "universal_ecommerce", "url": "https://www.redbubble.com/g/t-shirts"}'
+-d '{"source": "universal", "url": "https://www.redbubble.com/g/t-shirts"}'

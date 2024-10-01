d847e96a0177c4b3669fbac7f43c6fd9f8385002
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 12:50:26 +0300

    Update redbubble_python.py

--- code examples/redbubble_python.py
+++ code examples/redbubble_python.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 # Structure payload.
 payload = {
-    'source': 'universal_ecommerce',
+    'source': 'universal',
     'url': 'https://www.redbubble.com/g/t-shirts'
 }
 
